dfbdd904051feefe7f795a9b54f8c6fa2781970c
Eco <jerico.pulvera01z@gmail.com>
2018-11-28 23:04:41 +0800

    minor changes

--- pages/admin/reports/index.vue
+++ pages/admin/reports/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="admin-dashboard container mx-auto row">
-    <tabs class="tabular-view">
+    <!-- <tabs class="tabular-view">
       <tab title="All Categories">
         <div class="search pull-right">
           <form class="form-inline">
@@ -53,7 +53,7 @@
       <tab title="Emergency Management">
         <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
       </tab>
-    </tabs>
+    </tabs>-->
     <h1 class="title__black--large">Reports</h1>
     <table class="text-center w-100">
       <thead>
