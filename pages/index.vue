400a085418d363bce45c0159e8974f64bf84ce2f
Janno Reyes <reyesjanno@gmail.com>
2018-12-24 09:47:12 +0800

    new home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,32 @@
 <template>
   <div>
-    <section class="banner">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4 left-content">
-            <h2 class="title__blue--large text-uppercase">Report Now!</h2>
-            <div class="intro-text mb30">
-              <p
-                class="basic__dark"
-              >This website is an Incident Reporting App and Web Software, wherein you can manage and track incidents in a quick and effective way, inform the right people directly and effortlessly in just one click away</p>
-            </div>
-            <router-link to="/new-report" class="btn btnblue text-uppercase">Report</router-link>
-          </div>
-          <div class="col-md-8 right-content">
-            <img src="/img/monitor.png" alt class="img-responsive mrgnauto pull-right">
+      <div class="ads mt30">
+          <div class="container">
+              <img src="https://via.placeholder.com/1200x90.png?text=This+is+a+sample+ads" alt="" class="img-responsive mb20">
           </div>
         </div>
-      </div>
-    </section>
     <section class="main-content">
       <div class="news">
         <div class="container">
-          <h2 class="title__blue text-uppercase text-center mt0 mb45">News &amp; announcenement</h2>
+          <h2 class="title__blue text-uppercase text-center mt0 mb45">
+            News &amp; announcenement
+          </h2>
           <div class="row clrmargin">
             <div class="col-md-4 clrpadding item">
               <div class="panel">
                 <div class="panel-heading p0">
-                  <img src="/img/townships-01.jpg" alt class="img-responsive mrgnauto">
+                  <img
+                    src="/img/townships-01.jpg"
+                    alt
+                    class="img-responsive mrgnauto"
+                  />
                 </div>
                 <div class="panel-body">
                   <h2 class="title__black--lato mt0">Townships</h2>
-                  <p
-                    class="basic"
-                  >This 72-hectare township is envishione to be "the next central busines district in Iloilo.</p>
+                  <p class="basic">
+                    This 72-hectare township is envishione to be "the next
+                    central busines district in Iloilo.
+                  </p>
                 </div>
                 <div class="panel-footer p0">
                   <a href="#" class="btn btnmore pull-right">
@@ -63,13 +57,20 @@
             <div class="col-md-4 clrpadding item">
               <div class="panel">
                 <div class="panel-heading p0">
-                  <img src="/img/stmortiz-01.jpg" alt class="img-responsive mrgnauto">
+                  <img
+                    src="/img/stmortiz-01.jpg"
+                    alt
+                    class="img-responsive mrgnauto"
+                  />
                 </div>
                 <div class="panel-body">
-                  <h2 class="title__black--lato mt0">St. Moritz Private Estate</h2>
-                  <p
-                    class="basic"
-                  >With a residential scheme that brings out the spirit of glittering elegance known only to exist for the privileged.</p>
+                  <h2 class="title__black--lato mt0">
+                    St. Moritz Private Estate
+                  </h2>
+                  <p class="basic">
+                    With a residential scheme that brings out the spirit of
+                    glittering elegance known only to exist for the privileged.
+                  </p>
                 </div>
                 <div class="panel-footer p0">
                   <a href="#" class="btn btnmore pull-right">
@@ -100,13 +101,18 @@
             <div class="col-md-4 clrpadding item">
               <div class="panel">
                 <div class="panel-heading p0">
-                  <img src="/img/event.jpg" alt class="img-responsive mrgnauto">
+                  <img
+                    src="/img/event.jpg"
+                    alt
+                    class="img-responsive mrgnauto"
+                  />
                 </div>
                 <div class="panel-body">
                   <h2 class="title__black--lato mt0">Sample Title</h2>
-                  <p
-                    class="basic"
-                  >Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quia rem quibusdam veniam minus.</p>
+                  <p class="basic">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Expedita, quia rem quibusdam veniam minus.
+                  </p>
                 </div>
                 <div class="panel-footer p0">
                   <a href="#" class="btn btnmore pull-right">
@@ -137,13 +143,19 @@
             <div class="col-md-4 clrpadding item">
               <div class="panel">
                 <div class="panel-heading p0">
-                  <img src="/img/three central-01.jpg" alt class="img-responsive mrgnauto">
+                  <img
+                    src="/img/three central-01.jpg"
+                    alt
+                    class="img-responsive mrgnauto"
+                  />
                 </div>
                 <div class="panel-body">
                   <h2 class="title__black--lato mt0">Three Central</h2>
-                  <p
-                    class="basic"
-                  >Megaworld’s Three Central is an important residential condominium project located on the financial side of Ayala Avenue in Salcedo Village.</p>
+                  <p class="basic">
+                    Megaworld’s Three Central is an important residential
+                    condominium project located on the financial side of Ayala
+                    Avenue in Salcedo Village.
+                  </p>
                 </div>
                 <div class="panel-footer p0">
                   <a href="#" class="btn btnmore pull-right">
@@ -174,13 +186,18 @@
             <div class="col-md-4 clrpadding item">
               <div class="panel">
                 <div class="panel-heading p0">
-                  <img src="/img/mckinley-01.jpg" alt class="img-responsive mrgnauto">
+                  <img
+                    src="/img/mckinley-01.jpg"
+                    alt
+                    class="img-responsive mrgnauto"
+                  />
                 </div>
                 <div class="panel-body">
                   <h2 class="title__black--lato mt0">McKinley</h2>
-                  <p
-                    class="basic"
-                  >Megaworld's largest township development in Metro Manila, McKinley Hill.</p>
+                  <p class="basic">
+                    Megaworld's largest township development in Metro Manila,
+                    McKinley Hill.
+                  </p>
                 </div>
                 <div class="panel-footer p0">
                   <a href="#" class="btn btnmore pull-right">
@@ -211,13 +228,21 @@
             <div class="col-md-4 clrpadding item">
               <div class="panel">
                 <div class="panel-heading p0">
-                  <img src="/img/security-01.jpg" alt class="img-responsive mrgnauto">
+                  <img
+                    src="/img/security-01.jpg"
+                    alt
+                    class="img-responsive mrgnauto"
+                  />
                 </div>
                 <div class="panel-body">
-                  <h2 class="title__black--lato mt0">Security Force is Important</h2>
-                  <p
-                    class="basic"
-                  >Your security force is your eyes and ears during the busiest times of the day to when the world is asleep and you need a trustworthy sentry to make sure you’re protected.</p>
+                  <h2 class="title__black--lato mt0">
+                    Security Force is Important
+                  </h2>
+                  <p class="basic">
+                    Your security force is your eyes and ears during the busiest
+                    times of the day to when the world is asleep and you need a
+                    trustworthy sentry to make sure you’re protected.
+                  </p>
                 </div>
                 <div class="panel-footer p0">
                   <a href="#" class="btn btnmore pull-right">
@@ -251,16 +276,79 @@
           </div>
         </div>
       </div>
-      <div class="ads mt30">
+      <!-- <div class="ads mt30">
         <div class="container">
           <div class="row">
-            <div class="col-md-4 item" v-for="advertisement in advertisements" :key="`ad-${advertisement._id}`">
+            <div
+              class="col-md-4 item"
+              v-for="advertisement in advertisements"
+              :key="`ad-${advertisement._id}`"
+            >
               <div class="panel">
                 <div class="panel-heading">
-                  <a :href="advertisement.link" target="_blank" :class="[advertisement.link ? 'cursor-pointer':  'pointer-events-none' ]">
-                    <img :src="advertisement.image" alt class="img-responsive mrgnauto">
+                  <a
+                    :href="advertisement.link"
+                    target="_blank"
+                    :class="[advertisement.link ? 'cursor-pointer':  'pointer-events-none' ]"
+                  >
+                    <img
+                      :src="advertisement.image"
+                      alt
+                      class="img-responsive mrgnauto"
+                    />
                   </a>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> -->
+    </section>
+    <section class="banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 left-content">
+            <h2 class="title__blue--large text-uppercase">Report Now!</h2>
+            <div class="intro-text mb30">
+              <p class="basic__dark">
+                This website is an Incident Reporting App and Web Software,
+                wherein you can manage and track incidents in a quick and
+                effective way, inform the right people directly and effortlessly
+                in just one click away
+              </p>
+            </div>
+            <router-link to="/new-report" class="btn btnblue text-uppercase"
+              >Report</router-link
+            >
+          </div>
+          <div class="col-md-8 right-content">
+            <img
+              src="/img/monitor.png"
+              alt
+              class="img-responsive mrgnauto pull-right"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="container-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 left-content">
+            <div class="panel">
+              <div class="panel-body">
+                <h3 class="title__blue--large text-uppercase">Command center</h3>
+                <p class="basic">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, quod, error quae dolore officiis autem officia iure quibusdam aliquid, fugiat laboriosam maiores earum sint odio quisquam harum deserunt. Aliquam, magnam.</p>
+                <a href="#" class="btn btnblue">button</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 right-content">
+            <div class="panel">
+              <div class="panel-body">
+                <h3 class="title__blue--large text-uppercase">Help us improve our site</h3>
+                <textarea name="" id="" cols="30" rows="6" class="form-control mb20"></textarea>
+                <a href="#" class="btn btnblue">Submit</a>
               </div>
             </div>
           </div>
@@ -270,15 +358,15 @@
   </div>
 </template>
 <script>
-export default {
-  auth: false,
-  layout: "public",
-  asyncData ({ $axios }) {
-    return $axios.$get('/advertisements').then(response => {
-      return {
-        advertisements: response.data
-      }
-    })
-  }
-};
+  export default {
+    auth: false,
+    layout: "public",
+    asyncData({ $axios }) {
+      return $axios.$get("/advertisements").then(response => {
+        return {
+          advertisements: response.data
+        };
+      });
+    }
+  };
 </script>
