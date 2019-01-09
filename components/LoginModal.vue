8a48a172ef01a81238eeeb368175dfb76d74ff9a
Eco <jerico.pulvera01z@gmail.com>
2019-01-09 20:23:05 +0800

    minor changes

--- components/LoginModal.vue
+++ components/LoginModal.vue
@@ -29,10 +29,10 @@ export default {
   computed: {
     loginModal: {
       get() {
-        return this.$store.state.modals.login;
+        return this.$store.state.modals.login
       },
       set() {
-        return this.$store.commit("TOGGLE_LOGIN_MODAL");
+        return this.$store.commit('TOGGLE_LOGIN_MODAL')
       }
     }
   },
@@ -40,66 +40,67 @@ export default {
     return {
       loading: false,
       form: {
-        email: "admin@gmail.com",
-        password: "123123123"
+        email: 'admin@gmail.com',
+        password: '123123123'
       }
-    };
+    }
   },
   methods: {
     login() {
-      this.loading = true;
+      this.loading = true
       this.$auth
-        .loginWith("local", {
+        .loginWith('local', {
           data: {
             email: this.form.email,
             password: this.form.password
           }
         })
         .then(() => {
-          this.loading = false;
-          const permissions = this.$store.state.auth.user.role.permissions;
-          let redirectToHome = true;
+          this.loading = false
+          const permissions = this.$store.state.auth.user.role.permissions
+          let redirectToHome = true
+          this.$store.commit('TOGGLE_LOGIN_MODAL')
+
           for (let index = 0; index < permissions.length; index++) {
-            const permission = permissions[index];
+            const permission = permissions[index]
 
-            if (permission.name === "view command center") {
-              redirectToHome = false;
-              this.$router.push(`/command-center`);
-              break;
+            if (permission.name === 'view command center') {
+              redirectToHome = false
+              this.$router.push(`/command-center`)
+              break
             }
 
-            if (permission.name === "resolve") {
-              redirectToHome = false;
-              this.$router.push("/resolver");
-              break;
+            if (permission.name === 'resolve') {
+              redirectToHome = false
+              this.$router.push('/resolver')
+              break
             }
 
-            if (permission.name === "respond") {
-              redirectToHome = false;
-              this.$router.push("/responder");
-              break;
+            if (permission.name === 'respond') {
+              redirectToHome = false
+              this.$router.push('/responder')
+              break
             }
           }
 
           if (redirectToHome) {
-            this.$router.push("/");
+            this.$router.push('/')
           }
 
-          this.$store.commit("TOGGLE_LOGIN_MODAL");
-          this.$notify("Login successful!");
+          this.$notify('Login successful!')
         })
         .catch(errors => {
-          console.log(errors.response.data);
-          alert("Invalid Credentials!");
-          this.loading = false;
-        });
+          console.log(errors.response.data)
+          alert('Invalid Credentials!')
+          this.loading = false
+        })
     },
     socketConnect() {
       this.$socket.query.token = localStorage
-        .getItem("auth._token.local")
-        .replace("Bearer ", "");
-      this.$socket.connect();
+        .getItem('auth._token.local')
+        .replace('Bearer ', '')
+      this.$socket.connect()
     }
   }
-};
+}
 </script>
