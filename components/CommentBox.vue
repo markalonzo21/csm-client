3a643bacd6bbdf4edb50300deedc6db2d1a41427
Janno Reyes <reyesjanno@gmail.com>
2019-01-26 16:12:05 +0800

    style: working on comment area

--- components/CommentBox.vue
+++ components/CommentBox.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>Comment Here</div>
+<div class="panel panel-comment">
+  <div class="panel-body">
+    <div class="comment-item" v-for="i in 5">
+      <div class="avatar">
+        <a href="#">
+          <img alt="avatar" class="media-object" src="/img/avatar.jpg">
+        </a>
+      </div>
+      <div class="media-body">
+        <label class="label-name" for="">Responder's Name</label>
+        <span class="tiny pull-right">21 mins ago</span>
+        <small class="basic">
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. 
+        </small>
+      </div>
+    </div>
+  </div>
+  <div class="panel-footer mt10">
+    <form class="input-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Write something here ..."
+        >
+        <div class="input-group-btn">
+          <button
+            type="submit"
+            class="btn bluelabel text-uppercase"
+          >Send</button>
+        </div>
+      </form>
+  </div>
+</div>
+
 </template>
 
 <script>
@@ -13,3 +46,57 @@ export default {
   }
 }
 </script>
+<style>
+  .panel-comment .panel-body {
+    max-height: 300px;
+    overflow-y: auto;
+  }
+  .panel-comment .panel-body::-webkit-scrollbar-track
+{
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+	border-radius: 10px;
+	background-color: #F5F5F5;
+}
+
+.panel-comment .panel-body::-webkit-scrollbar
+{
+	width: 8px;
+	background-color: #F5F5F5;
+}
+
+.panel-comment .panel-body::-webkit-scrollbar-thumb
+{
+	border-radius: 10px;
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+	background-color: #555;
+}
+  .avatar {
+    float: left;
+    margin-right: 10px;
+  }
+  small.basic {
+    display: inline-block;
+  }
+  .comment-item {
+    margin-bottom: 10px;
+  }
+  .avatar img {
+    border-radius: 50%;
+    object-fit: cover;
+    height: 50px;
+    width: 50px;
+  }
+
+  .panel-comment .panel-footer {
+    margin-top: 10px;
+    background-color: #fff;
+  }
+  .bluelabel {
+  color: #34c3e5;
+  font-weight: 700;
+}
+.tiny {
+  color: #ccc;
+}
+</style>
+
--- pages/report-tracker/_id/index.vue
+++ pages/report-tracker/_id/index.vue
@@ -1,105 +1,158 @@
 <template>
   <div class="responder active-report main-content" v-if="report">
     <div class="container">
+      <h1 class="title__black mt0 uppercase">Report ID #</h1>
+      <div class="row">
       <div class="active-report">
         <div class="col-md-6">
-          <div class="border rounded bg-white py-6 px-6">
-            <div class="clearfix">
-              <span class="font-semibold text-blue-dark float-left">Date</span>
-              <span
-                class="float-right"
-              >{{$moment(report.createdAt).format("MMM. DD, YYYY | h:mm A ")}}</span>
-            </div>
-            <div class="clearfix" v-if="report.resolvedAt">
-              <span class="font-semibold text-blue-dark float-left">Date Resolved</span>
-              <span
-                class="float-right"
-              >{{$moment(report.resolvedAt).format("MMM. DD, YYYY | h:mm A ")}}</span>
-            </div>
-            <div class="clearfix">
-              <span class="font-semibold text-blue-dark float-left">Category</span>
-              <span class="float-right">{{ report.type.category.name }}</span>
-            </div>
-            <div class="clearfix">
-              <span class="font-semibold text-blue-dark float-left">Type</span>
-              <span class="float-right">{{ report.type.name }}</span>
-            </div>
-            <div class="clearfix">
-              <span class="font-semibold text-blue-dark float-left">Reporter</span>
-              <span
-                class="float-right"
-                v-if="report.reporter"
-              >{{ report.reporter.firstName }} {{ report.reporter.middleName }} {{ report.reporter.lastName }} ({{ report.reporter.mobile }})</span>
-            </div>
-            <div class="clearfix">
-              <span class="font-semibold text-blue-dark float-left">Resolver</span>
-              <span
-                class="float-right"
-                v-if="report.resolver"
-              >{{ report.resolver.firstName }} {{ report.resolver.middleName }} {{ report.resolver.lastName }} ({{ report.resolver.mobile }})</span>
-            </div>
-            <div class="clearfix">
-              <span class="font-semibold text-blue-dark float-left">Responder</span>
-              <span
-                class="float-right"
-                v-if="report.responder"
-              >{{ report.responder.firstName }} {{ report.responder.middleName }} {{ report.responder.lastName }} ({{ report.responder.mobile }})</span>
-              <span v-else>None</span>
-            </div>
+          <div class="panel">
+            <div class="panel-body">
+              <table class="table">
+                <tr>
+                  <td>
+                    <span class="bluelabel">Date</span>
+                  </td>
+                  <td>
+                    <span class="basic">{{$moment(report.createdAt).format("MMM. DD, YYYY | h:mm A ")}}</span>
+                  </td>
+                  <td>
+                  </td>
+                </tr>
 
-            <div class="clearfix">
-              <span class="font-semibold text-blue-dark float-left">Status</span>
-              <span class="float-right">{{ report.status}}</span>
+                <tr>
+                  <td>
+                    <span class="bluelabel">Date Resolved</span>
+                  </td>
+                  <td>
+                    <span class="basic">{{$moment(report.resolvedAt).format("MMM. DD, YYYY | h:mm A ")}}</span>
+                  </td>
+                  <td>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <span class="bluelabel">Category</span>
+                  </td>
+                  <td>
+                    <span class="basic">{{ report.type.category.name }}</span>                    
+                  </td>
+                  <td>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <span class="bluelabel">Type</span>
+                  </td>
+                  <td>
+                    <span class="basic">{{ report.type.name }}</span>
+                  </td>
+                  <td>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <span class="bluelabel">Reporter</span>
+                  </td>
+                  <td>
+                    <span class="basic" v-if="report.reporter">{{ report.reporter.firstName }} {{ report.reporter.middleName }} {{ report.reporter.lastName }} ({{ report.reporter.mobile }})</span>
+                  </td>
+                  <td>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <span class="bluelabel">Resolver</span>
+                  </td>
+                  <td>
+                    <span class="basic" v-if="report.resolver">{{ report.resolver.firstName }} {{ report.resolver.middleName }} {{ report.resolver.lastName }} ({{ report.resolver.mobile }})</span>
+                    <span class="basic" v-else>None</span>
+                  </td>
+                  <td>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <span class="bluelabel">Responder</span>
+                  </td>
+                  <td>
+                    <span class="basic" v-if="report.responder">{{ report.responder.firstName }} {{ report.responder.middleName }} {{ report.responder.lastName }} ({{ report.responder.mobile }})</span>
+                    <span class="basic" v-else>None</span>
+                  </td>
+                  <td>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <span class="bluelabel">Status</span>
+                  </td>
+                  <td>
+                    <span class="basic">{{ report.status}}</span>                    
+                  </td>
+                  <td>
+                  </td>
+                </tr>
+                
+              </table>
             </div>
           </div>
-          <div class="border rounded bg-white py-6 px-6 mt-8">
-            <div class="clearfix mt-4">
-              <div class="font-semibold text-blue-dark">Notes</div>
-              <p>{{ report.description }}</p>
+          <div class="panel panel-notes">
+            <div class="panel-body">
+                <div class="bluelabel">Notes</div>
+                <p class="basic">{{ report.description }}</p>
             </div>
           </div>
-          <div class="border rounded bg-white py-6 px-6 mt-8" v-if="report.media.length > 0">
-            <div class="font-semibold text-blue-dark">Media</div>
-            <div class="col-md-3" v-for="media in report.media" :key="media">
-              <img :src="media" alt="image-media" v-if="$utils.isImage(media)">
-              <video width="300" controls v-else>
-                <source :src="media" type="video/mp4">
-              </video>
+          
+          <div class="panel panel-media">
+            <div class="panel-body" v-if="report.media.length > 0">
+              <div class="font-semibold text-blue-dark">Media</div>
+              <div class="col-md-3" v-for="media in report.media" :key="media">
+                <img :src="media" alt="image-media" v-if="$utils.isImage(media)">
+                <video width="300" controls v-else>
+                  <source :src="media" type="video/mp4">
+                </video>
+              </div>
             </div>
           </div>
         </div>
 
-        <div class="col-md-6">
-          <div class="border rounded bg-white py-6 px-6" v-if="report.location !== null">
-            <h4 class="font-bold mt-0 text-blue-darker">Incident Location</h4>
-            <div id="map-wrap" style="height: 200px; width: 100%;" class="mt-4">
-              <no-ssr>
-                <l-map
-                  :center="report.map.center"
-                  :zoom="report.map.zoom"
-                  :minZoom="report.map.minZoom"
-                  :maxZoom="report.map.maxZoom"
-                  ref="map"
-                >
-                  <l-tile-layer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-                  <l-marker :lat-lng="report.map.center"></l-marker>
-                </l-map>
-              </no-ssr>
+        <div class="col-md-6 right-content">
+          <div class="panel panel-location">
+            <div class="panel-body" v-if="report.location !== null">
+              <span class="bluelabel">Location Map</span>
+              <div id="map-wrap" style="height: 200px; width: 100%;" class="mt-4">
+                <no-ssr>
+                  <l-map
+                    :center="report.map.center"
+                    :zoom="report.map.zoom"
+                    :minZoom="report.map.minZoom"
+                    :maxZoom="report.map.maxZoom"
+                    ref="map"
+                  >
+                    <l-tile-layer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+                    <l-marker :lat-lng="report.map.center"></l-marker>
+                  </l-map>
+                </no-ssr>
+              </div>
             </div>
           </div>
-
-          <div class="border rounded bg-white py-6 px-6 mt-8">
-            <CommentBox :type="'user'"/>
-          </div>
+          <CommentBox :type="'user'"/>
         </div>
       </div>
+      </div>
+      
     </div>
   </div>
 </template>
 
 
 <script>
-import CommentBox from '~/components/CommentBox'
+import CommentBox from "~/components/CommentBox";
 
 export default {
   components: {
@@ -107,34 +160,38 @@ export default {
   },
   async asyncData({ store, error, params, redirect }) {
     try {
-      await store.dispatch('user/getReport', params.id)
+      await store.dispatch("user/getReport", params.id);
     } catch (err) {
-      error({ status: err.response.status, message: 'Report not found!' })
+      error({ status: err.response.status, message: "Report not found!" });
     }
   },
   computed: {
     report() {
-      return this.$store.state.user.report
+      return this.$store.state.user.report;
     }
   },
   mounted() {
-    this.initSocketListener()
+    this.initSocketListener();
   },
   beforeDestroy() {
-    this.$socket.off('report-updated')
+    this.$socket.off("report-updated");
   },
   methods: {
     initSocketListener() {
-      this.$socket.on('report-updated', report => {
-        this.$store.commit('responder/SET_REPORT', report)
-      })
+      this.$socket.on("report-updated", report => {
+        this.$store.commit("responder/SET_REPORT", report);
+      });
     }
   }
-}
+};
 </script>
 
 <style scoped>
 .rounded {
   border-radius: 1rem;
+}
+.bluelabel {
+  color: #34c3e5;
+  font-weight: 700;
 }
 </style>
