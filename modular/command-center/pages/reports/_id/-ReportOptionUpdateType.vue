6d0737d4ca066709871325af7dda23ae0fae5218
Eco <jerico.pulvera01z@gmail.com>
2019-03-01 19:13:18 +0800

    feat: working on update report type

--- modular/command-center/pages/reports/_id/-ReportOptionUpdateType.vue
+++ modular/command-center/pages/reports/_id/-ReportOptionUpdateType.vue
@@ -1,43 +1,103 @@
 <template>
-   <div class="row">
+  <div class="row">
     <div class="col-md-3">
       <label for>Type</label>
     </div>
     <div class="col-md-6">
       <input
-        type="text"
         :disabled="true"
-        class="form-control"
         :value="report.type.name"
+        class="form-control"
+        type="text"
       >
     </div>
     <div class="col-md-3">
-      <button class="btn btnform" @click.prevent="alertItsNotWorkingYet">Change</button>
+      <button
+        @click.prevent="updateModalVisible = true"
+        class="btn btnform"
+      >Change</button>
     </div>
+
+    <!-- MODAL -->
+    <modal
+      :class="{ 'pointer-events-none': loadingUpdate }"
+      :header="false"
+      class="assign-modal"
+      v-model="updateModalVisible"
+    >
+      <div class="row text-center">
+        <h3 class="mt-0 mb-6">Update Report Type</h3>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div v-if="loadingGetReportCategories">Loading...</div>
+          <select
+            class="form-control"
+            required
+            v-else
+            v-model="currentType"
+          >
+            <option :value="null">Select Report Type</option>
+            <option
+              :key="category._id"
+              :value="category._id"
+              v-for="category in categories"
+            >{{ category.name }}</option>
+          </select>
+        </div>
+      </div>
+      <div
+        class="text-center"
+        slot="footer"
+      >
+        <button
+          :disabled="!loadingUpdate"
+          @click.prevent="changeType"
+          class="btn btn-primary float-right"
+          style="width: auto;"
+        >Save</button>
+      </div>
+    </modal>
   </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      report: {
-        type: Object,
-        required: true
-      }
-    },
-    data() {
-      return {
-        currentType: this.report.type
-      }
-    },
-    methods: {
-      alertItsNotWorkingYet() {
-        alert('Reassigning type is still under development')
-      },
-      changeType() {
-
-      }
+export default {
+  props: {
+    report: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted() {
+    this.loadingGetReportCategories = true;
+    this.$axios.$get("/report-categories").then(response => {
+      this.categories = response.data;
+      this.loadingGetReportCategories = false;
+    });
+  },
+  data() {
+    return {
+      categories: [],
+      loadingUpdate: false,
+      loadingGetReportCategories: false,
+      updateModalVisible: false,
+      currentType: this.report.type ? this.report.type._id : null
+    };
+  },
+  methods: {
+    changeType() {
+      this.$axios
+        .$post("/admin/v1/update-report-type", {
+          type: this.currentType,
+          reportId: this.report._id
+        })
+        .then(response => {
+          this.loadingUpdate = false;
+          this.updateModalVisible = false;
+        })
+        .catch(error => {});
     }
   }
+};
 </script>
-
