45f7484e6189bf7caf3fde8a27a05cec582620b2
Janno Reyes <reyesjanno@gmail.com>
2019-03-21 23:47:48 +0800

    style: terms and forgot

---
+++ pages/forgot-password.vue
@@ -0,0 +1,40 @@
+<template>
+<section class="user-profile main-content">
+<div class="container">
+    <h2 class="title__blue">Forgot Password</h2>
+    <div class="panel">
+        <div class="panel-body">
+            <label for="" class="bluelabel">Enter email address</label><br />
+            <input class="form-control" type="email" name="" id="">
+                    <button class="btn btnblue">Reset Password</button>
+
+        </div>
+    </div>
+    </div>
+</section>
+    
+    
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.panel {
+  box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.75);
+}
+.panel-heading {
+  background-color: #fff;
+}
+.table {
+  margin-bottom: 0;
+}
+.bluelabel {
+  color: #34c3e5;
+}
+
+.title__blue {
+  font-weight: 700;
+}
+</style>
---
+++ pages/terms.vue
@@ -0,0 +1,38 @@
+<template>
+<section class="user-profile main-content">
+<div class="container">
+    <h2 class="title__blue">Terms and Condition</h2>
+    <div class="panel">
+        <div class="panel-body">
+            <p class="basic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore earum nihil soluta facilis assumenda consequatur, sequi id dignissimos corporis in ratione laudantium repudiandae architecto voluptate esse alias voluptates possimus iure facere, neque error omnis et! Voluptatem molestiae quo nisi distinctio expedita? Illo eum possimus cum veniam doloribus maiores molestiae molestias.</p>
+            <p class="basic">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt hic facere error iusto corrupti quidem totam quo expedita sunt saepe.</p>
+        </div>
+    </div>
+    </div>
+</section>
+    
+    
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.panel {
+  box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.75);
+}
+.panel-heading {
+  background-color: #fff;
+}
+.table {
+  margin-bottom: 0;
+}
+.bluelabel {
+  color: #34c3e5;
+}
+
+.title__blue {
+  font-weight: 700;
+}
+</style>
