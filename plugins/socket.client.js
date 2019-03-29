31931109091fc3e746329584c253471b53dc1ced
Eco <jerico.pulvera01z@gmail.com>
2019-03-29 18:29:34 +0800

    fix: on premise env example

--- plugins/socket.client.js
+++ plugins/socket.client.js
@@ -2,22 +2,19 @@ import io from 'socket.io-client'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export default function (ctx, inject) {
-  const socket = io(
-    process.env.API_PROXY ? process.env.API_PROXY_TARGET : process.env.API_URL,
-    {
-      reconnect: true,
-      forceNew: true,
-      transports: ['websocket'],
-      query: {
-        token: window.localStorage.getItem('auth._token.local')
-          ? window.localStorage
+export default function(ctx, inject) {
+  const socket = io(process.env.API_URL, {
+    reconnect: true,
+    forceNew: true,
+    transports: ['websocket'],
+    query: {
+      token: window.localStorage.getItem('auth._token.local')
+        ? window.localStorage
             .getItem('auth._token.local')
             .replace('Bearer ', '')
-          : ''
-      }
+        : ''
     }
-  )
+  })
 
   socket.on('connect', () => {
     if (process.env.NODE_ENV === 'development') console.log('socket connected')
