f6541c9e6d2e0c6d3637e7e9fc0f0aaca6af6fb8
Eco <jerico.pulvera01z@gmail.com>
2019-04-01 20:40:08 +0800

    fix:  wrong dev:live script

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 
-export default function ({ $axios, app }) {
+export default function({ $axios, app }) {
   $axios.onRequest(config => {
-    // console.log(config.url)
+    console.log(config.url)
     if (!process.env.API_URL) {
       config.url = `${config.url}`
     }
@@ -17,6 +17,7 @@ export default function ({ $axios, app }) {
     ) {
       app.$auth.reset()
     }
-    // console.log({ 'error-response': error.response })
+    // console.log({ 'error-response': error })
+    console.log({ 'error-response': error.response.data })
   })
 }
